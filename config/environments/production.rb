6db4fa6c50785b9fa7632fea4e3f4a406e1d4c92
Jakub Novotny <novotny.jakub@gmail.com>
2014-11-07 18:59:12 +0000

    Allow Sendgrid domain to be set using ENV

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
     authentication: :plain,
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
+    domain: ENV['SENDGRID_DOMAIN'] || 'people-finder.dsd.io',
     enable_starttls_auto: true
   }
 
