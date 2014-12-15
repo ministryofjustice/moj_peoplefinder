390173bbde0130391b1511f3bf27858ac57ac963
Jakub Novotny <novotny.jakub@gmail.com>
2014-12-15 15:07:51 +0000

    Change force_ssl settings and add protocol to mailer
    SSL has been forced from within the engine until now. This moves that responsibility to the wrapper and can later be removed from the engine

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = {
     host: 'localhost',
-    port: 3000
+    port: 3000,
+    protocol: 'http'
   }
 end
