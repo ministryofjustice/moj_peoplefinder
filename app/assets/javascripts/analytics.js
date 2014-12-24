d191ee3e23049d96350b285c27080d94c3c2625a
Mehmet Duran <mehmet.duran@gmail.com>
2014-12-24 17:56:52 +0000

    Added analytics

--- app/assets/javascripts/analytics.js
+++ app/assets/javascripts/analytics.js
@@ -1,5 +1,38 @@
 /* global $ */
 
 $(function(){
+  'use strict';
+
+  var ifPage = function (klass, f){
+    if( $(document.body).hasClass(klass) && f instanceof Function ){
+      f();
+    }
+  }
+
+  var sendEvent = function ( selector, ev, page, eventLabel, text ){
+    $(selector)[ev](function (){
+        // ga('send', 'event', 'search-index-page', 'next-button', 'Next button clicked to start');
+        ga('send', 'event', page, eventLabel, text);
+    });
+  }
+
+
+  ifPage('login-page', function (){
+    sendEvent('input.button', 'click', 'login-page', 'login-with-email', 'Clicked on "Log in with email" button.');
+    sendEvent('a.login-button', 'click', 'login-page', 'login-with-google', 'Clicked on "Log in with Google" button.');
+  });
+
+
+  ifPage('home-page', function (){
+    ga('send', 'event', 'home-page', 'looking-at-home-page', 'User is looking at the home page');
+
+    sendEvent('search-box', 'home-page', 'home-page-search', 'User made a search from the home page');
+  });
+
+  ifPage('search-page', function (){
+    ga('send', 'event', 'search-page', 'looking-at-search-page', 'User is looking at the search page');
+
+
+  });
 
 });
