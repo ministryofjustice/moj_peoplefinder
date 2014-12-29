1426b43d2a0e974d9cdf04488b4a0b9528d9ea8e
Mehmet Duran <mehmet.duran@gmail.com>
2014-12-29 11:14:46 +0000

    Analytics implemented

--- app/assets/javascripts/analytics.js
+++ app/assets/javascripts/analytics.js
@@ -12,7 +12,8 @@ $(function(){
   var sendEvent = function ( selector, ev, page, eventLabel, text ){
     $(selector)[ev](function (){
         // ga('send', 'event', 'search-index-page', 'next-button', 'Next button clicked to start');
-        ga('send', 'event', page, eventLabel, text);
+        if( ga && ga instanceof Function)
+          ga('send', 'event', page, eventLabel, text);
     });
   }
 
@@ -26,13 +27,59 @@ $(function(){
   ifPage('home-page', function (){
     ga('send', 'event', 'home-page', 'looking-at-home-page', 'User is looking at the home page');
 
-    sendEvent('search-box', 'home-page', 'home-page-search', 'User made a search from the home page');
+    sendEvent('form.search-box', 'submit', 'home-page', 'home-page-search', 'User made a search from the home page');
+    sendEvent('.leaf-node a.subteam-link', 'click', 'home-page', 'home-page-used-the-org-browser', 'User went to a team page from the org browser');
+    sendEvent('.add-new-person', 'click', 'home-page', 'home-page-add-new-profile', 'User clicked on add new profile');
+
+    $('.new-org-browser').click(function (ev){
+      var $self = $(ev.target).closest('.new-org-browser');
+      if( !$self.hasClass('tracked-for-click') ){
+        ga('send', 'event', 'home-page', 'home-page-explored-the-org-browser', 'User clicked on the org browser');
+        $self.addClass('tracked-for-click');
+      }
+    });
   });
+
 
   ifPage('search-page', function (){
     ga('send', 'event', 'search-page', 'looking-at-search-page', 'User is looking at the search page');
 
+    var values = [
+      { label: 'search-page-0-results', text: 'No results found for keyword' },
+      { label: 'search-page-1-result',  text: '1 result found for keyword' },
+      { label: 'search-page-2-results', text: '2 results found for keyword' }
+    ];
 
+    var multiValue = {
+      label: 'search-page-multiple-results', text: 'More than two results found for keyword'
+    };
+
+    var resultCount = $('.search-result').length;
+
+    if( resultCount < 3 ){
+      ga('send', 'event', 'search-page', values[resultCount].label, values[resultCount].text);
+    } else {
+      ga('send', 'event', 'search-page', multiValue.label, multiValue.text);
+    }
+
+
+    var clickValues = [
+      { label: 'search-page-click-first-item',  text: 'User clicked on the first search result' },
+      { label: 'search-page-click-second-item', text: 'User clicked on the second search result' },
+      { label: 'search-page-click-third-item',  text: 'User clicked on the third search result' },
+      { label: 'search-page-click-past-third-item',  text: 'User clicked on a search result past the third one' }
+    ];
+
+    $('.search-result a').click(function (ev){
+      var $result = $(ev.target).closest('.search-result');
+      var index = $('.search-result').index($result);
+
+      if( index < 3 ){
+        ga('send', 'event', 'search-page', clickValues[index].label, clickValues[index].text);
+      } else {
+        ga('send', 'event', 'search-page', clickValues[3].label, clickValues[3].text);
+      }
+    });
   });
 
 });
