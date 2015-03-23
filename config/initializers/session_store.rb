b1048d1c706a8bb184b6370c0110d90d1b42506c
Jonathan Conway <jonathan@jaikoo.com>
2015-03-23 18:00:21 +0000

    Set secure cookies for staging/production
    Wasn't able to do a blanket setting for all production environments because of our mixed demo/staging/production envs.
    
    Also of note I've set the max age of these cookies to 3 hours.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
-# Be sure to restart your server when you modify this file.
+SERVICE_DOMAIN = (url = ENV['ACTION_MAILER_DEFAULT_URL']) ? URI.parse(url).host : nil
 
-Rails.application.config.session_store :cookie_store, key: '_moj_peoplefinder_session'
+{key: '_moj_peoplefinder_session', expire_after: 1.day, httponly: true, max_age: 1.day.to_s, domain: SERVICE_DOMAIN}.tap do |configuration|
+  configuration[:secure] = (ENV['SSL_ON'] != 'false' ? true : false)
+  Peoplefinder::Application.config.session_store :cookie_store, configuration
+end
