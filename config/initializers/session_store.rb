d5bbaf8a9747055747fe85baeb347d35d94e7dc2
Jonathan Conway <jonathan@jaikoo.com>
2015-03-23 18:01:03 +0000

    Set max age of the cookies to 3 hours

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 SERVICE_DOMAIN = (url = ENV['ACTION_MAILER_DEFAULT_URL']) ? URI.parse(url).host : nil
 
-{key: '_moj_peoplefinder_session', expire_after: 1.day, httponly: true, max_age: 1.day.to_s, domain: SERVICE_DOMAIN}.tap do |configuration|
+{key: '_moj_peoplefinder_session', expire_after: 3.hours, httponly: true, max_age: 3.hours.to_s, domain: SERVICE_DOMAIN}.tap do |configuration|
   configuration[:secure] = (ENV['SSL_ON'] != 'false' ? true : false)
   Peoplefinder::Application.config.session_store :cookie_store, configuration
 end
