faaf28e751aa3ea924187dd40572c90be3e49686
Jonathan Conway <jonathan@jaikoo.com>
2015-03-23 15:20:50 +0000

    Don't force ssl for health check route

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,8 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and
   # use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
+  config.ssl_options = { exclude: proc { |env| env['PATH_INFO'] == "/ping" } }
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
